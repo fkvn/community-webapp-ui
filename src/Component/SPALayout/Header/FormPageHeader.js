ec6113880b5580cc4c3910bb4cfb8b5d68fefe48
Kevin Ngo <phucaone@gmail.com>
2023-11-23 21:54:23 -0800

    Update FormPageHeader.js

--- src/Component/SPALayout/Header/FormPageHeader.js
+++ src/Component/SPALayout/Header/FormPageHeader.js
@@ -6,36 +6,40 @@ import { REDIRECT_URI } from "../../../Util/constVar";
 import useImage from "../../Hook/useImage";
 import SwitchLanguage from "../../Locale/SwitchLanguage";
 
-function FormPageHeader({
-	props = {},
-	onBeforeClose = () => Promise.resolve(),
-}) {
+function FormPageHeader({ onBeforeClose = () => Promise.resolve() }) {
 	const navigate = useNavigate();
 	const [params] = useSearchParams();
-	const { image } = useImage();
 	const redirectUri = params.get(REDIRECT_URI) || "";
+
+	const { image } = useImage();
 
 	const App = () => (
 		<>
 			<Flex justify="space-between" align="center" className="my-1 mx-3">
-				{image({
-					src: svgThaiNowLogoWithWords,
-					width: 50,
-					className: "",
-					onClick: () => navigate("/"),
-					style: {
-						cursor: "pointer",
-					},
-				})}
+				<Flex justify="space-between" align="center">
+					{image({
+						src: svgThaiNowLogoWithWords,
+						width: 50,
+						className: "",
+						onClick: () => navigate("/"),
+						style: {
+							cursor: "pointer",
+						},
+					})}
+				</Flex>
+
 				<Flex justify="space-between" align="center">
 					<SwitchLanguage />
 					<Button
-						className="border-0 pt-2 "
-						icon={<CloseOutlined className="text-danger" />}
+						type="primary"
+						className="border-0 px-2 mx-2 custom-center bg-customRed"
+						size="medium"
+						icon={<CloseOutlined className="text-white" />}
 						onClick={() =>
 							onBeforeClose().then(() => navigate(`/${redirectUri}`))
 						}
-					/>
+						style={{ fontSize: "1rem" }}
+					></Button>
 				</Flex>
 			</Flex>
 			<Divider className="m-0 p-0" />
